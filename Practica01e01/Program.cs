795983ef380f49a7e473a2b75e5556fefaaadabc
AReyes <AReyes@MASTERSERVER>
2021-08-19 11:38:50 -0400

    Add project files.

---
+++ Practica01e01/Program.cs
@@ -0,0 +1,32 @@
+// Practica 1 Ejercicio 1. 
+// Nota : En el proyecto modifique la página de indice para que diga wellcome [primer nombre]
+//
+//
+//                                           © By Anthony Reyes Duran
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica01e01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
